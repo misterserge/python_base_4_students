8c9929310272076444f26d0fd4a5c69cb89c7f1f
Serge Gusarov <mrhersergik@gmail.com>
2025-02-20 21:50:25 +0700

    memory address

--- extend/data_structure/main.py
+++ extend/data_structure/main.py
@@ -1,6 +1,9 @@
 # dynamic type
 var_name = 1
 var_name = True
+type(var_name) # get type of the var
+bool(var_name) # convert to boll (error here)
+id(var_name) # get the memory address of the var
 
 # func names
 def get_name():
