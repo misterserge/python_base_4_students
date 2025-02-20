7be9d0ad3f30d7979e9bae9c361f1b7fd5e062d1
Serge Gusarov <mrhersergik@gmail.com>
2025-02-20 21:03:39 +0700

    variables

---
+++ extend/variables/main.py
@@ -0,0 +1,10 @@
+# rules
+CONST_DATA # UPPERCASE - constants
+var_name # snake_case - vars/funcions/methods/modules
+Class_Name # Pascal_Case - classes
+package-name # packages
+
+# assingment
+var_name = 1
+var_name = True
+print(var_name) # True
