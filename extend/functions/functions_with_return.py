e796978120c8338fd2ce3d2fc8542fa36ad1d717
Serge Gusarov <mrhersergik@gmail.com>
2025-02-20 20:13:13 +0700

    finished functions

--- extend/functions/functions_with_return.py
+++ extend/functions/functions_with_return.py
@@ -1,6 +1,11 @@
 def sum_nums(a, b):
     sum = a + b
-    print(sum)
+    # print(sum)
     return sum
+    print('line ignored')
 
-print(sum_nums(1, 2)) # 3
+
+print(sum_nums(10, 2.5))
+result = sum_nums(1, 2)  # 3
+print(result)
+print(sum_nums(sum_nums(10, 2.5), 30))
