2f6a03a8b34cfb86b4ac58fa7a0fe6d4cff2cc6f
Serge Gusarov <mrhersergik@gmail.com>
2025-02-20 20:08:57 +0700

    functions

---
+++ extend/functions/functions.py
@@ -0,0 +1,9 @@
+def hello(var='111'):
+    print('hello ', var)
+    return "hello"
+
+
+print('hello from main level would be 1st')
+
+hello('User')
+hello()
---
+++ extend/functions/functions_with_return.py
@@ -0,0 +1,6 @@
+def sum_nums(a, b):
+    sum = a + b
+    print(sum)
+    return sum
+
+print(sum_nums(1, 2)) # 3
