3aa05c140ca7b28042864d832244e0b48113ed90
Serge Gusarov <mrhersergik@gmail.com>
2025-02-21 15:37:52 +0700

    strings init

--- extend/data_structure/main.py
+++ extend/data_structure/main.py
@@ -5,6 +5,10 @@ type(var_name) # get type of the var
 bool(var_name) # convert to boll (error here)
 id(var_name) # get the memory address of the var
 
+print(id(var_name))
+second = var_name
+print(id(second)) # same memory address
+
 # func names
 def get_name():
      # here we started with verb/action and the context
