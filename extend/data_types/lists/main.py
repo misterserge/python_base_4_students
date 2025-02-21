3e0dbece8c3f7ea9d03225a87f677d743f4d6733
Serge Gusarov <mrhersergik@gmail.com>
2025-02-21 18:44:32 +0700

    lists

---
+++ extend/data_types/lists/main.py
@@ -0,0 +1,73 @@
+list_items = ['true', True, 0]
+print(list_items)
+del list_items[-1]
+print(list_items)
+
+users = [
+    {
+        'id': 1,
+        'name': 'John Doe',
+    },
+    {
+        'id': 2,
+        'name': 'Bob',
+    }
+]
+print(users)
+users.append({
+    'id': 3,
+    'name': 'Alice',
+})
+print(users)
+print(len(users))
+print(id(users))
+print(type(users))
+print(dir(users))
+print(users[0].get('id'))
+
+copy_users = users
+print(copy_users)
+copy_users = users.copy()
+print(copy_users)
+copy_users.pop()
+print(copy_users)
+copy_users.pop(0)
+print(copy_users)
+
+
+new_list_items = [1, 5, 6, 3, 2, 4, 7, 8, 9]
+new_list_items.append(11)
+new_list_items.insert(1, 1)
+print(max(new_list_items))
+new_list_items.sort()
+print(new_list_items)
+new_list_items.sort(reverse=True)
+print(new_list_items)
+new_list_items.reverse()
+new_list_items.clear()
+
+# convert
+string_ = "hey, hi from here"
+list_ = list(string_)
+print(list_)
+list_ = list({
+    'id': 1,
+    'name': 'John Doe',
+})
+print(list_)
+print(list_+new_list_items[:2])
+list_cop = list_
+print(id(list_), id(list_cop))
+print(list_, list_cop)
+list_cop = list_.copy()
+print(id(list_), id(list_cop))
+print(list_, list_cop)
+list_cop = list_[:]
+print(id(list_), id(list_cop))
+print(list_, list_cop)
+list_cop = [*list_]
+print(id(list_), id(list_cop))
+print(list_, list_cop)
+list_cop = list(list_)
+print(id(list_), id(list_cop))
+print(list_, list_cop)
