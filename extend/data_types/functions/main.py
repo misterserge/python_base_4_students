a4b3254481d0473f0bc053a6e17d16a6480fbb61
Serge Gusarov <mrhersergik@gmail.com>
2025-03-27 21:27:26 +0700

    functions finished

--- extend/data_types/functions/main.py
+++ extend/data_types/functions/main.py
@@ -8,3 +8,11 @@ person_one = {
 }
 print(person_one)
 print(increase_person_age(person_one))
+
+def merge_list_to_dict(my_list, my_list2):
+    return dict(zip(my_list, my_list2))
+
+print(merge_list_to_dict(['name', 'age'], ['Alice', 25]))
+def merge_dicts(dict1, dict2):
+    return {**dict1, **dict2}
+print(merge_dicts({'name': 'Alice'}, {'age': 25}))
